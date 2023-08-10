29b2549d7b91c623fb707db77c1274fc0b4bf506
jaovy05 <joaovitorg.silva050@gmail.com>
2023-08-10 00:47:18 -0300

    Condições part 1

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,4 @@
+var vel = 45;
+console.log(`Você está na velocidade ${vel}Km/h.`)
+if (vel > 60) console.log(`Você está MULTADO por passar a velocidade permitida.`)
+console.log(`Sempre dirija com cinto de segurança`)
---
+++ javascript/aula11/ex009.js
@@ -0,0 +1,4 @@
+var pais = 'EUA';
+console.log(`Nasci em ${pais}`);
+if (pais != 'Brasil') console.log(`Você é estrangeiro!`);
+else console.log(`Você é Brasileiro`);
