5e8523414892361759d797e44b0bae329796ba46
jaovy05 <joaovitorg.silva050@gmail.com>
2023-08-13 18:25:23 -0300

    agora o select mostra todas opções

--- javascript/aula14ex/ex017/script.js
+++ javascript/aula14ex/ex017/script.js
@@ -1,18 +1,15 @@
 function tabuar() {
     var num = document.getElementById("num").value;
-    var tabela = document.getElementById("tabela");
-    
-    if (num === "") {
+    var tabela = document.getElementById("tabela"); 
+    if (num.length == "0") {
         alert("Por favor, digite um número.");
     } else {
         num = Number(num);
-        tabela.innerHTML = ""; // Limpa o conteúdo anterior da tabela
-        
+        tabela.innerHTML = ""; 
         for (var i = 1; i <= 10; i++) {
             var opcao = document.createElement("option"); 
-            opcao.value = i;
+            opcao.value = `op${i}`;
             opcao.textContent = `${num} * ${i} = ${num * i}`;
             tabela.appendChild(opcao);
-    }
-}
+    }}
 }
